f3054035b0a73210a25642ece79805ca794cf269
speccy-rom <speccy.rom@gmail.com>
2022-01-27 22:02:31 +0300

    add conf: settings for ORM tortoise and migrations

--- project/app/main.py
+++ project/app/main.py
@@ -1,9 +1,19 @@
-#!/bin/bash
+import os
+
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from .config import get_settings, Settings
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
