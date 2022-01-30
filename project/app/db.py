605b1b24508a9bf230c80e6a45f71ad3db78552c
speccy-rom <speccy.rom@gmail.com>
2022-01-30 23:50:30 +0300

    fixed: refactoring app

--- project/app/db.py
+++ project/app/db.py
@@ -1,5 +1,8 @@
 import os
 
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
 
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
@@ -10,3 +13,13 @@ TORTOISE_ORM = {
         },
     },
 }
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
